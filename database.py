b4a6e4c7a9344d013398b9d6346e108b95a0ba97
Mrinal Raj <mrinalraj2809@gmail.com>
2023-10-23 00:59:00 +0530

    Added new databse credentials

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://4nv2jvc6dx72256cg07d:pscale_pw_GGBIzRIR2sEAsPvdhWHrvH9CvNUkQODA1uqBOxoqceV@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://y5tgeaux3lr0l278v729:pscale_pw_IdrfOa2xqxJ6g64TSoaC5UAutcC4beGLDZ0sUpLOJCf@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
 
 engine = create_engine(
      db_connection_string,
