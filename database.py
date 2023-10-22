9392a183986e0825754cca13a0d8266d26560b15
Mrinal Raj <mrinalraj2809@gmail.com>
2023-10-23 01:35:49 +0530

    Added new database credentials

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://y5tgeaux3lr0l278v729:pscale_pw_IdrfOa2xqxJ6g64TSoaC5UAutcC4beGLDZ0sUpLOJCf@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://wamdf02tnwplgc75v7sq:pscale_pw_FvZvNHQTRA6xZIYl7IqEo7jFmMuBj1Y7G3KtGhQc1hk@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
 
 engine = create_engine(
      db_connection_string,
