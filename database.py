270d8aa380b706d10a96493bbb49da0981057d38
Mrinal Raj <mrinalraj2809@gmail.com>
2023-10-23 01:46:52 +0530

    Edited the password

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://wamdf02tnwplgc75v7sq:pscale_pw_FvZvNHQTRA6xZIYl7IqEo7jFmMuBj1Y7G3KtGhQc1hk@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
+db_connection_string = "mysql+pymysql://krifxfdh9vfyxfw8twa4:pscale_pw_NHGnZeETMeeaoA7cAtPvBPRWprL4XpPN7mJWVEhVhOi@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
 
 engine = create_engine(
      db_connection_string,
