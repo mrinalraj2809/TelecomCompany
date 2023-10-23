5dd4ece401b6cf2d7aa11e4d33077a5a0eac03f3
Mrinal Raj <mrinalraj2809@gmail.com>
2023-10-23 12:10:57 +0530

    Implmented Env variable

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, text
-
-db_connection_string = "mysql+pymysql://krifxfdh9vfyxfw8twa4:pscale_pw_NHGnZeETMeeaoA7cAtPvBPRWprL4XpPN7mJWVEhVhOi@aws.connect.psdb.cloud/telecomcompany?charset=utf8mb4"
+from dotenv import load_dotenv
+import os
+load_dotenv()
+db_connection_string = os.getenv("DB_CONNECTION_STRING")
 
 engine = create_engine(
      db_connection_string,
