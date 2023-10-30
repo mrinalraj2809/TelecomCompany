6d69c437676f56e880b8c9c40497d94adb886945
Mrinal Raj <mrinalraj2809@gmail.com>
2023-10-30 07:52:41 +0530

    Added Servie Requests Features

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database import engine, text
 
 app = Flask(__name__)
 
+####### DB CONNECTIONS ######
 def load_data_from_db(query):
     with engine.connect() as conn:
         result = conn.execute(text(query))
@@ -23,19 +24,40 @@ def get_subscriptions(c_id):
                              INNER JOIN subscriptions\
                              ON subscriptions.plan_id = plans.plan_id\
                              where subscriptions.customer_id=1;")
+def get_service_request():
+    return load_data_from_db("select * from servicerequests;")
 
+####### DB CONNECTIONS ENDS######
+
+
+####### Populate HOME PAGE STARTS######
 @app.route("/")
 def home():
     # return "Hello, World!"
     plans = get_plans()
     customers = get_customers()
     subscriptions = get_subscriptions(1)
-    return render_template('home.html', plans=plans, customers=customers, subscriptions=subscriptions)
+    service_requests = get_service_request()
+    return render_template('home.html', plans=plans, customers=customers, subscriptions=subscriptions,service_requests=service_requests)
+    # return render_template('home.html', plans=plans, customers=customers, subscriptions=subscriptions)
 
+####### Populate HOME PAGE ENDS######
+
+
+####### Populate PLANS PAGE STARTS######
 @app.route("/api/plans")
-def list_plans():
+def list_plans_in_json():
     # return "Hello, World!"
     return jsonify(PLANS)
 
+@app.route("/Pricing")
+def list_plans():
+    plans = get_plans()
+    return render_template('planitems.html', plan=plans)
+####### Populate PLANS ENDS STARTS######
+
+
+
+###### MAIN FUNCTION########
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
