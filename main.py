ec9bbc59099b645027f353b339ec70e75167e7e2
Mrinal Raj (mriraj) <mriraj@cisco.com>
2023-11-04 00:39:37 +0530

    Added functionality to service requests buttons

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import engine, text
+from flask import Flask, render_template, jsonify, request
+from database import engine, text, load_new_service_request_to_db, get_customer_details_from_db
 
 app = Flask(__name__)
 
@@ -31,9 +31,8 @@ def get_service_request():
 
 
 ####### Populate HOME PAGE STARTS######
-@app.route("/")
+@app.route("/", methods=['get'])
 def home():
-    # return "Hello, World!"
     plans = get_plans()
     customers = get_customers()
     subscriptions = get_subscriptions(1)
@@ -43,21 +42,51 @@ def home():
 
 ####### Populate HOME PAGE ENDS######
 
-
-####### Populate PLANS PAGE STARTS######
-@app.route("/api/plans")
-def list_plans_in_json():
-    # return "Hello, World!"
-    return jsonify(PLANS)
-
 @app.route("/Pricing")
 def list_plans():
     plans = get_plans()
     return render_template('planitems.html', plan=plans)
 ####### Populate PLANS ENDS STARTS######
 
+# login #
+@app.route("/login", methods=['post', 'get'])
+def login():
+    if request.method == 'post' or request.method == 'get':
+        return jsonify(request.args)
+    return render_template('login.html')
 
+# sign up #
+@app.route("/signup", methods=['post', 'get'])
+def signup():
+    if request.method == 'get':
+        data = request.form
+        return jsonify(data)
+        load_new_customer_into_db(data)
+        return render_template('login.html')
+    return render_template('signup.html')
+
+# raise service request #
+@app.route("/new/request", methods=['post', 'get'])
+def create_new_request():
+    return render_template('raiseservicerequest.html')
+
+#New Service Reqeuest Form Data#
+@app.route("/new/request/raise", methods=['post', 'get'])
+def add_service_request_to_db():
+    # data = request.form
+    # return jsonify(data)
+    # print("DATA:",data)
+    data = {}
+    data['description'] = 'Internet Issue'
+    request_number = load_new_service_request_to_db(data)
+    # print(request_number)
+    plans = get_plans()
+    customers = get_customers()
+    subscriptions = get_subscriptions(1)
+    service_requests = get_service_request()
+    return render_template('home.html', plans=plans, customers=customers, subscriptions=subscriptions,
+                           service_requests=service_requests)
 
 ###### MAIN FUNCTION########
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='127.0.0.1',debug=True)
